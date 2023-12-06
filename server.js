260f7d09b4ba50a4d727ed42a45e8a05f1156c70
WeenieMelon <manuelsurkc@gmail.com>
2023-12-06 16:33:42 -0600

    fixed file types, updated server,js, created highscore page

--- server.js
+++ server.js
@@ -10,3 +10,18 @@ var pool = mysql.createPool({
   database: "rockpaperscissors",
   connectionLimit:10
 });
+app.set('view-engine','ejs')
+
+app.get('/',function(reg,res){
+    res.render('RockPaperScissors.ejs')
+})
+app.get('/highscore',function(reg,res){
+    pool.query('SELECT high_score.score,high_score.username FROM high_score ORDER BY score',function(err,rows){
+        if (err) throw err;
+        if(!err){
+            console.log(rows.length);
+            res.render('highscore.ejs',{rows});
+        }
+    })
+})
+
